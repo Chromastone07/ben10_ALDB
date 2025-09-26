112a95996bc2611e3383dbf9d00c0741e4d1dbe5
chromastone07 <okappeal@gmail.com>
2025-09-26 17:47:09 +0530

    new

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('*', (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
